4dc8c13a04b101eda0685b23dc6f2d757a7e051b
Akshitha-jangam <s210594@rguktsklm.ac.in>
2024-07-09 22:14:27 +0530

    Update level2.c

--- level2.c
+++ level2.c
@@ -246,3 +246,27 @@ int main()
     printf("%d",count);
     }
 //===========================================================================================================================================
+
+
+//12)Question: Write a program to get a number from user and print the sum of all digits.
+//Answer: Input: 123456 - Output – 21
+//Input: 76895439 - Output – 51
+//Input: 675 – Output - 18
+#include<stdio.h>
+int main()
+{
+    int a,num,sum=0;
+    printf("Enter the number");
+    scanf("%d",&num);
+    loop : if(num!=0)
+    {
+        a=num%10;
+        sum=sum+a;
+        num=num/10;
+        goto loop;
+    }
+    printf("%d",sum);
+}
+//===========================================================================================================================================
+
+
