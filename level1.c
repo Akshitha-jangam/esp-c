e0a0dbb55d332bc06ac54c201de184983e2d93c5
Akshitha-jangam <s210594@rguktsklm.ac.in>
2024-12-10 10:53:14 +0530

    Update level1.c

--- level1.c
+++ level1.c
@@ -135,6 +135,20 @@ int main()
   c=b%10;
   printf("the ten's digit is %d",c);
 }
+
+
+                                                    (OR)
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the three digit number");
+    scanf("%d",&a);
+    b= a%100;
+    c= b/10;
+    printf("%d",c);
+}
+
 //=========================================================================================================================
 
 
